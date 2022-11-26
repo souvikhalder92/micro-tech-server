ce155b79cd7b304161ea8737e4cb9852c54c2634
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-11-27 02:57:06 +0600

    ok

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ async function run(){
 
         const categoriesCollection = client.db('microTech').collection('categories');
         const productsCollection = client.db('microTech').collection('products');
+        const bookingsCollection = client.db('microTech').collection('bookings');
        
         app.get('/categories',async(req,res) =>{
             const query={};
@@ -34,7 +35,7 @@ async function run(){
             const query = {_id: ObjectId(id)};
             const result = await categoriesCollection.findOne(query);
             res.send(result);
-        })
+        });
         app.get('/products',async(req,res) =>{
             console.log(req.query.categoryName);
             let query = {};
@@ -47,13 +48,26 @@ async function run(){
             const cursor = productsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
-        })
+        });
         app.get('/products/:id',async(req,res) =>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await productsCollection.findOne(query);
             res.send(result);
+        });
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email; 
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
         })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await  bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
         
       
     }
