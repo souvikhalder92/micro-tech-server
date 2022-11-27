2876955fd66133ebdf5dae73945e322e94eda296
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-11-27 11:50:53 +0600

    ok

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require( 'express');
 const cors = require( 'cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +25,7 @@ async function run(){
         const categoriesCollection = client.db('microTech').collection('categories');
         const productsCollection = client.db('microTech').collection('products');
         const bookingsCollection = client.db('microTech').collection('bookings');
+        const usersCollection = client.db('microTech').collection('users');
        
         app.get('/categories',async(req,res) =>{
             const query={};
@@ -67,6 +69,29 @@ async function run(){
             console.log(booking);
             const result = await  bookingsCollection.insertOne(booking);
             res.send(result);
+        });
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '30d' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
         });
         
       
